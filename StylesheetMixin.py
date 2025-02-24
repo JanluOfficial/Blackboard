5ceed0fe7b964b82cdc067ccb05f49d942e0d19a
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2025-02-24 19:10:17 +0100

    Update custom path to not have a forced underscore

--- StylesheetMixin.py
+++ StylesheetMixin.py
@@ -14,7 +14,7 @@ class StylesheetMixin:
     def get_default_stylesheet_path(self, prefix: str = "custom"):
         # Determine the base path (works with frozen executables)
         base_path = sys._MEIPASS if hasattr(sys, 'frozen') else os.path.dirname(__file__)
-        custom_path = os.path.join(base_path, f'{prefix}_style.qss')
+        custom_path = os.path.join(base_path, f'{prefix}style.qss')
         # Ensure self.settings exists before accessing it
         if os.path.exists(custom_path) and getattr(self, 'settings', None) and self.settings.get('UI', 'custom_theming'):
             return custom_path
